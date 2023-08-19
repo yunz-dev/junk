e0c67badb35079c5a7854dd37b4630643cdbf196
Yuunzz <112053367+yuunzz@users.noreply.github.com>
2023-08-20 03:14:08 +1000

    linked list js half done

--- first_recurring_char.js
+++ first_recurring_char.js
@@ -1,13 +1,12 @@
 function findFirst(arrayOfChars) {
     history = {}
     for (letter in arrayOfChars) {
-        if (!history[arrayOfChars[letter]]) {
-
-            history[arrayOfChars[letter]] = 1;     
-        } else {
+        if (history[arrayOfChars[letter]]) {
             return arrayOfChars[letter];
+        } else {
+            history[arrayOfChars[letter]] = 1;     
         }
     }
 }
 
-console.log(findFirst([1,2,3,1,2,3]));
+console.log(findFirst([1,2,3,2,1,2,3]));
---
+++ linkedlist.js
@@ -0,0 +1,43 @@
+// add a method prepend() to the linked list that adds a node to the beginning of the list
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+   const newNode = {
+            value: value,
+            next: null
+        }; 
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+
+console.log(myLinkedList);
+
+
+
+
