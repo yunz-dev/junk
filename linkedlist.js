fe4269fcccf8e62c77bd20b23f7612bc3d98f155
Yuunzz <112053367+yuunzz@users.noreply.github.com>
2023-08-21 22:21:31 +1000

    updated linked list

--- linkedlist.js
+++ linkedlist.js
@@ -1,4 +1,4 @@
-// add a method insert() to the linked list that adds a node to the specified index.
+// add a method remove() to the linked list that deletes a node to the specified index.
 
 class LinkedList {
   constructor(value) {
@@ -39,29 +39,48 @@ class LinkedList {
     return array;
   }
   insert(index, value){
-    //Code here
-    let currentNode = this.head;
-    for (let i = 1; i < index; i++) {
-      currentNode = currentNode.next;  
+    //Check for proper parameters;
+    if(index >= this.length) {
+      console.log('yes')
+      return this.append(value);
     }
+    
     const newNode = {
-        value: value,
-        next: currentNode.next
+      value: value,
+      next: null
     }
-    currentNode.next = newNode;
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
     this.length++;
     return this.printList();
+  }
+  remove(index){
+    const leader =  this.traverseToIndex(index - 1);
+    delete this.traverseToIndex(index);
+    const leader3 =  this.traverseToIndex(index + 1);
+    leader.next = leader3;
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
   }
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
-myLinkedList.append(16);
-myLinkedList.prepend(1);
+myLinkedList.append(16);myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
 console.log(myLinkedList.printList());
-console.log(myLinkedList.insert(2, 99));
-console.log(myLinkedList.insert(99,99));
-
-
-
-
+myLinkedList.remove(2);
+console.log(myLinkedList.printList());
+myLinkedList.remove(3);
+console.log(myLinkedList.printList());
